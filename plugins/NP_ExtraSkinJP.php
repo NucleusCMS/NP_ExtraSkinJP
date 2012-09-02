b8cdf25df5a6da1d40b7546d1ddf265d9d0ca46e
yama <yamamoto@kyms.ne.jp>
2012-09-02 14:22:53 +0900

    Replace ereg function (for PHP 5.3)

--- plugins/NP_ExtraSkinJP.php
+++ plugins/NP_ExtraSkinJP.php
@@ -124,8 +124,8 @@ class NP_ExtraSkinJP extends NucleusPlugin {
 	function init()
 	{
 		// include language file for this plugin
-		$language = ereg_replace( '[\\|/]', '', getLanguageName());
-		$incFile  = (file_exists($this->getDirectory().$language.'.php')) ? $language : 'english';
+		$language = str_replace( array('\\','/'), '', getLanguageName());
+		$incFile  = (is_file($this->getDirectory().$language.'.php')) ? $language : 'english';
 		include_once($this->getDirectory().$incFile.'.php');
 		$this->language = $incFile;
 	}
